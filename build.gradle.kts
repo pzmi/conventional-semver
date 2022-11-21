a48bb09e87c0cd8ab6122cadd70381e96faecf3c
Patryk Żmigrodzki <pzmi@users.noreply.github.com>
2022-11-21 22:30:51 +0100

    Add simple experiments
    Signed-off-by: Patryk Żmigrodzki <pzmi@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "dev.zmigrodzki"
+group = "dev.zmigrodzki.conventionalsemver"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
--- src/main/java/dev/zmigrodzki/conventionalsemver/Main.java
+++ src/main/java/dev/zmigrodzki/conventionalsemver/Main.java
@@ -1,7 +1,42 @@
-package dev.zmigrodzki;
+package dev.zmigrodzki.conventionalsemver;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        var builder = new FileRepositoryBuilder()
+                .readEnvironment()
+                .findGitDir();
+        try (var repository = builder.build()) {
+            var git = new Git(repository);
+            var latestTag = git.describe().setAbbrev(0).setTags(true).setTarget("master").call();
+            var walk = new RevWalk(repository);
+            var from = repository.resolve("refs/heads/master");
+            var to = repository.resolve(Constants.R_TAGS + latestTag);
+
+            walk.markStart(walk.parseCommit(from));
+            walk.markUninteresting(walk.parseCommit(to));
+
+            for (RevCommit revCommit : walk) {
+                var shortMessage = revCommit.getShortMessage();
+                var split = shortMessage.split(":", 2);
+                if (split.length > 1) {
+                    var bump = switch (split[0]) {
+                        case "feat!" -> "major";
+                        case "feat" -> "minor";
+                        default -> "bugfix";
+                    };
+                    System.out.println(split[0]);
+                    System.out.println(bump);
+                }
+
+                revCommit.disposeBody();
+            }
+        }
+
     }
 }
